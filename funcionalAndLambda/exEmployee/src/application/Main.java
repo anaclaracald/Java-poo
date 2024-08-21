c7e32c0e2ef8e554e4ec4b767f41fca65c8c5c30
Ana Clara <ana05clara24@gmail.com>
2024-08-20 22:03:04 -0300

    Files of the fixing exercise

---
+++ funcionalAndLambda/exEmployee/src/application/Main.java
@@ -0,0 +1,42 @@
+package application;
+
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter full file path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Employee> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            System.out.print("Enter the minimum salary: ");
+            double minSalary = sc.nextDouble();
+
+            list.stream()
+
+
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
+}
